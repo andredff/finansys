577775c75cab018f165f60b74fbd87522c5cef11
Andre Souza <andre.souza@solucioneonline.com.br>
2019-10-02 02:08:36 -0300

    adding base methods to CategoryForm && adding sweetAlert toastService

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { ToastService } from './shared/toast.service';
 
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
-  ]
+    CategoriesRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [ToastService],
+
 })
 export class CategoriesModule { }
